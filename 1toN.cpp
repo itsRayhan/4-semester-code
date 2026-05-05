c8194f524d8b73f30cf157c0e535ab252a369b76
itsRayhan <itsrayhaninbox@gmail.com>
2026-05-06 01:33:13 +0600

    6/5/2026 end

--- 1toN.cpp
+++ 1toN.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
-int main)()
+int main()
 {
     int n;
-    int
+    int i = 0, table;
+
+    cout << "enter the number";
+    cin >> n;
+
+    for (i = 1; i <= 10; i++)
+    {
+
+        table = i * n;
+        cout << n << " x " << i << " = " << table << endl;
+        if (i >= n) break;
+    }
+    cout << "run code" << table << endl;
+
+    return 0;
 }
