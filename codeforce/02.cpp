ad8232ac1cdca80641f6824254f3c04e129f47f6
itsRayhan <itsrayhaninbox@gmail.com>
2026-08-22 03:45:05 +0600

    2

---
+++ codeforce/02.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "dfjkxd";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        string word;
+        cin >> word;
+
+        if (word.length() > 10)
+        {
+            cout << word[0]
+                 << word.length() - 2
+                 << word[word.length() - 1]
+                 << endl;
+        }
+        else
+        {
+            cout << word << endl;
+        }
+    }
+
+    return 0;
+}
